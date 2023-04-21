ebd80515f6d4d6528ed57665f516f7414e604288
lucascmpus <lucascmpus@gmail.com>
2023-04-21 13:27:38 -0300

    refactor: import router and change port

--- src/app.py
+++ src/app.py
@@ -1,27 +1,12 @@
-from fastapi import FastAPI, Depends
-from sqlalchemy.orm import Session
-from pydantic import BaseModel, List
-from src.schemas.schemas import Product
-from src.infra.sqlalchemy.config.database import get_db, create_db
-from src.infra.sqlalchemy.repositories.repositoryProduct import RepositoryProduct
+import uvicorn
+from src.router import router
 
 create_db()
 
 app = FastAPI()
 
-
-@app.post('/products')
-async def create_product(product: Product, db: Session = Depends(get_db)):
-    created_product = RepositoryProductd(db).create(self, product)
-    return created_product
+app.include_router(router.router)
 
 
-@app.get('/products')
-async def listProducts():
-    products = RepositoryProduct(db).getAll()
-    return products
-
-
-@app.get('/user')
-async def create_user(user: User, db: Session = Depends(get_db)):
-    created_user = RepositoryUser(db).create(self, user)
+if __name__ == '__main__':
+    uvicorn.run(app, port=8000)
