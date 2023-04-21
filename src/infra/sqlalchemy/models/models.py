619127912e73f71dce8ad33d91eff3db8a5bea79
lucascmpus <lucascmpus@gmail.com>
2023-04-21 13:27:04 -0300

    feat: create order table

--- src/infra/sqlalchemy/models/models.py
+++ src/infra/sqlalchemy/models/models.py
@@ -1,5 +1,7 @@
-from sqlalchemy import Column, Integer, String, Float, Boolean, Date
+from sqlalchemy import Column, Integer, String, Float, Boolean, Date, ForeignKey
 from src.infra.sqlalchemy.config.database import Base
+from sqlalchemy.orm import Mapped, mapped_column, DeclarativeBase, relationship
+
 
 class Product():
 
@@ -10,3 +12,31 @@ class Product():
     description = Column(String)
     price = Column(Float)
     avaivable = Column(Boolean)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship("User", backref=backref("user", uselist=False))
+
+
+class User():
+
+    __tablename__ = 'user'
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String)
+    tel = Column(String)
+
+
+class Order():
+
+    __tablename__ = 'order'
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship("User", backref=backref("user", uselist=False))
+    quantity = Column(Integer)
+    delivery = Column(Boolean)
+    address = Column(String)
+    obs = Column(String, nullable=True)
+
+
+class Sales():
+
+    __tablename__ = 'sales'
