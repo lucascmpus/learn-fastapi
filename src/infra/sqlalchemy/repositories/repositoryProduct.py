d5bc0f37b13c5cb1fd971af90d83cc30cdb2c165
lucascmpus <lucascmpus@gmail.com>
2023-04-22 14:57:52 -0300

    feat: finish repositories user and product

--- src/infra/sqlalchemy/repositories/repositoryProduct.py
+++ src/infra/sqlalchemy/repositories/repositoryProduct.py
@@ -1,4 +1,5 @@
 from sqlalchemy.orm import Session
+from sqlalchemy import select, delete
 from src.schemas import schemas as s
 from src.infra.sqlalchemy.models import models as m
 
@@ -20,8 +21,14 @@ class RepositoryProduct():
         products = self.db.query(m.Product).all()
         return products
 
-    def getOne(self):
-        pass
+    def getOne(self, product_id: int):
+        statement = select(m.Product).filter_by(id=product_id)
+        product = self.db.execute(statement).one()
 
-    def remove(self):
-        pass
+        return product
+
+    def remove(self, product_id: int):
+        statement = self.getOne(product_id).where(m.Product.id == product_id)
+
+        self.db.delete(statement)
+        self.db.commit()
--- src/infra/sqlalchemy/repositories/repositoryUser.py
+++ src/infra/sqlalchemy/repositories/repositoryUser.py
@@ -1,4 +1,5 @@
 from sqlalchemy.orm import Session
+from sqlalchemy import select, delete
 from src.schemas import schemas as s
 from src.infra.sqlalchemy.models import models as m
 
@@ -7,15 +8,28 @@ class RepositoryUser():
     def __init__(self, db: Session):
         self.db = db
 
-    def create(self):
-        db_user = m.User
-        return
+    def create(self, user: s.User):
+        db_user = m.User(
+            name=user.name, tel=user.tel
+        )
+
+        self.db.add(db_user)
+        self.db.commit()
+        self.db.refresh(db_user)
+        return db_user
 
     def getAll(self):
-        return
+        users = self.db.query(m.User).all()
+        return users
 
-    def getOne(self):
-        return
+    def getOne(self, user_id: int):
+        statement = select(m.User).filter_by(id=user_id)
+        user = self.db.execute(statement).one()
 
-    def remove(self):
-        return
+        return user
+
+    def remove(self, user_id: int):
+        statement = self.getOne(user_id).where(m.User.id == user_id)
+
+        self.db.delete(statement)
+        self.db.commit()
